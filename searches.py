f74ef3800db0094986918e18e1fad2220c65a440
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-05-09 17:13:12 -0300

    add merge sort big O n log n

--- searches.py
+++ searches.py
@@ -1,30 +1,39 @@
 # Searches can be linear or logarithmic
 
+unsorted_array_1 = [10, 7, 8, 3, 4, 9, 5, 1, 2]
+sorted_array_1 = [1, 2, 3, 4, 5, 7, 8, 9, 10]
+sorted_array_2 = [1, 2, 3]
+
 # Linear Search - O(n)
 # Works better with non sorted lists
 def Linear_Search(arr, value):
         for element in arr:
                 if(element == value):
-                        print("Value Found at position ", element)
+                        return "Value Found at position ", element
         
-        print("Not Found")
+        return "Not Found"
 
 
 # Binary Search - O(log n)
 # Works better with sorted lists
 def Binary_Search(arr, value):
         start_index = 0
-        end_index = len(arr - 1)
+        end_index = len(arr) - 1
 
-        while (start_index < end_index):
+        while (start_index <= end_index):
                 middle_index = int((start_index + end_index) / 2)
 
                 if (arr[middle_index] == value):
-                        print("Value Found at position", middle_index)
+                        return "Value Found at position", middle_index
 
                 if(arr[middle_index] < value):
                         start_index = middle_index + 1
                 else:
                         end_index =  middle_index - 1
         
-        print("Not Found")
+        return "Not Found"
+
+
+
+print Linear_Search(unsorted_array_1, 4)
+print Binary_Search(sorted_array_1, 4)
--- sorts.py
+++ sorts.py
@@ -1,9 +1,58 @@
 # Sorts can use insertion or comparison
 
+some_array_1 = [10, 7, 8, 3, 4, 9, 5, 1, 2]
+some_array_2 = [10, 7, 8, 3, 4, 9, 5, 1, 2]
+some_array_3 = [10, 7, 8, 3, 4, 9, 5, 1, 2]
+some_array_4 = [10, 7, 8, 3, 4, 9, 5, 1, 2]
+
 # Comparison Sorts
+
+# Merge Sort - O(n log n)
+def Merge_Sort(arr):
+        if len(arr) <= 1:
+                return
+
+        # Divide and Conquer 
+        mid_element = len(arr) // 2
+        left_half = arr[:mid_element]
+        right_half = arr[mid_element:]
+
+        # Recursively Sorting the Halves
+        Merge_Sort(left_half)
+        Merge_Sort(right_half)
+
+        # Initialize Counters
+        i = j = k = 0
+
+        # Comparison
+        while i < len(left_half) and j < len(right_half):
+                if left_half[i] < right_half[j]:
+                        arr[k] = left_half[i]
+                        i += 1
+                else:
+                        arr[k] = right_half[j]
+                        j += 1
+                k += 1
+        
+        # Check if any element was left behind
+        while i < len(left_half):
+                arr[k] = left_half[i]
+                i += 1
+                k += 1
+        
+        while j < len(right_half):
+                arr[k] = right_half[j]
+                j += 1
+                k += 1
+
+        return arr
+
 
 # Bubble Sort - O(n^2)
 def Bubble_Sort(arr):
+        if len(arr) <= 1:
+                return
+
         for i in arr:
                 for j in range(len(arr) - 1):
                         if (arr[j] > arr[j + 1]):
@@ -11,10 +60,13 @@ def Bubble_Sort(arr):
                                 arr[j + 1] = arr[j]
                                 arr[j] = lesser
 
-        print(arr)
+        return arr
 
 # Selection Sort - O(n^2)
 def Selection_Sort(arr):
+        if len(arr) <= 1:
+                return
+
         for i in range(len(arr)):
                 min_index = i
                 
@@ -27,13 +79,17 @@ def Selection_Sort(arr):
                         arr[min_index] = arr[i]
                         arr[i] = lesser
 
-        print(arr)
+        return arr
 
 
-# Insertion Sort
+
+# Insertion Sorts
 
 # Insertion Sort - O(n^2)
 def Insertion_Sort(arr):
+        if len(arr) <= 1:
+                return
+
         for i in range(len(arr)):
                 key = arr[i]
                 j = i - 1
@@ -44,4 +100,11 @@ def Insertion_Sort(arr):
 
                 arr[j + 1] = key
 
-        print(arr)
+        return arr
+
+
+
+print Merge_Sort(some_array_1)
+print Bubble_Sort(some_array_2)
+print Selection_Sort(some_array_3)
+print Insertion_Sort(some_array_4)
