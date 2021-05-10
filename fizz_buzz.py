5653d820f06230664d6078e7ec20c855045738d6
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-05-10 14:40:57 -0300

    chenge fizz buZZ

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,5 +1,5 @@
 # Fizz Buzz
-def Fizz_Buss(end_number):
+def Fizz_Buzz(end_number):
         for i in range(0, end_number):
                 if i % 5 == 0 and i % 3 == 0:
                         print("FizzBuzz")
@@ -10,23 +10,23 @@ def Fizz_Buss(end_number):
                 else:
                         print(i)
 
-def Improved_Fizz_Buss(end_number):
+def Improved_Fizz_Buzz(end_number):
         for j in range(0, end_number):
                 output = ''
                 if j % 3 == 0:
                         output += 'Fizz'
                 if j % 5 == 0:
-                        output += 'Buss'
+                        output += 'Buzz'
 
                 print(output or j)
 
-def Minified_Fizz_Buss(end_number):
+def Minified_Fizz_Buzz(end_number):
         for k in range(0, end_number):
                 output = 'Fizz' if k % 3 == 0 else ''
-                output += 'Buss' if k % 5 == 0 else ''
+                output += 'Buzz' if k % 5 == 0 else ''
 
         print(k) if output == '' else print(output)
 
-Fizz_Buss(101)
-Improved_Fizz_Buss(101)
-Minified_Fizz_Buss(101)
+Fizz_Buzz(101)
+Improved_Fizz_Buzz(101)
+Minified_Fizz_Buzz(101)
