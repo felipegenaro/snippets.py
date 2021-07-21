7e42a52dc593a85d8646cad9d6bc1ac74c63c48b
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-07-21 13:27:23 -0300

    add pywhatkit to automation algorithms

---
+++ handwrite_text.py
@@ -0,0 +1,6 @@
+# PyWhatKit is an automation lib
+# You can use to send WhatsApp msg, play YT videos and make Google searches
+
+import pywhatkit
+
+pywhatkit.text_to_handwriting("Lorem Ipsum", rgb=(0,0,0))
