7e21d61cd5992315c64a6e2271ee7953a2647b25
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-07-21 13:25:04 -0300

    rank time

--- searches.py
+++ searches.py
@@ -1,15 +1,25 @@
+import time as t
+
 # Searches can be linear or logarithmic
 
-unsorted_array_1 = [10, 7, 8, 3, 4, 9, 5, 1, 2]
-sorted_array_1 = [1, 2, 3, 4, 5, 7, 8, 9, 10]
-sorted_array_2 = [1, 2, 3]
+unsorted_array = [10, 7, 8, 3, 4, 9, 5, 1, 2]
+sorted_array = [1, 2, 3, 4, 5, 7, 8, 9, 10]
+
+# From this list, in classification of the better and most constant algorithm 
+# 1 - Binary_Search on Sorted List
+# 2 - Linear_Search on Unsorted List
+# 3 - Linear_Search on Sorted List
+# 4 - Binary_Search on Unsorted List
 
 # Linear Search - O(n)
 # Works better with non sorted lists
 def Linear_Search(arr, value):
+        start_time = t.time()
+
         for element in arr:
                 if(element == value):
-                        return "Value Found at position ", element
+                        end_time = t.time() - start_time
+                        return "Value Found at position ", element, end_time
         
         return "Not Found"
 
@@ -17,6 +27,8 @@ def Linear_Search(arr, value):
 # Binary Search - O(log n)
 # Works better with sorted lists
 def Binary_Search(arr, value):
+        start_time = t.time()
+
         start_index = 0
         end_index = len(arr) - 1
 
@@ -24,7 +36,8 @@ def Binary_Search(arr, value):
                 middle_index = int((start_index + end_index) / 2)
 
                 if (arr[middle_index] == value):
-                        return "Value Found at position", middle_index
+                        end_time = t.time() - start_time
+                        return "Value Found at position", middle_index, end_time
 
                 if(arr[middle_index] < value):
                         start_index = middle_index + 1
@@ -35,5 +48,7 @@ def Binary_Search(arr, value):
 
 
 
-print Linear_Search(unsorted_array_1, 4)
-print Binary_Search(sorted_array_1, 4)
+print "Linear_Search on Unsorted List", Linear_Search(unsorted_array, 4)
+print "Linear_Search on Sorted List", Linear_Search(sorted_array, 4)
+print "Binary_Search on Unsorted List", Binary_Search(unsorted_array, 4)
+print "Binary_Search on Sorted List", Binary_Search(sorted_array, 4)
